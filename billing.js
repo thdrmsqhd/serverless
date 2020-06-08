b16796f8732cc1848382b04bfe14377e688ee4c0
song <thdrmsqhd@gmail.com>
2020-06-08 20:56:44 +0900

    billing api test

--- billing.js
+++ billing.js
@@ -3,17 +3,18 @@ import handler from "./libs/handler-lib";
 import { calculateCost } from "./libs/billing-lib";
 
 export const main = handler(async (event, context) => {
-    const { storage, source } = JSON.parse(event.body);
-    const amount = calculateCost(storage);
-    const description = "Scratch charge";
+  const { storage, source } = JSON.parse(event.body);
+  const amount = calculateCost(storage);
+  const description = "Scratch charge";
 
-    const stripe = stripePackage(process.env.stripeSecretKey);
+  // Load our secret key from the  environment variables
+  const stripe = stripePackage(process.env.stripeSecretKey);
 
-    await stripe.charges.create({
-        source,
-        amount,
-        description,
-        currency: "usd"
-    });
-    return { status: true };
+  await stripe.charges.create({
+    source,
+    amount,
+    description,
+    currency: "usd"
+  });
+  return { status: true };
 });
